d4430b64a5f428b0413096d4f430580ab05d2b93
Soufiane KHAIT <soufiane.khait@alumni.enac.fr>
2021-05-28 20:05:13 +0200

    add global variables

--- config.py
+++ config.py
@@ -7,3 +7,7 @@ FT2M = 0.3048           # feet to meter
 NM2M = 1852             # nautical mile to meter
 KTS2MS = NM2M/3600      # knot to m/s
 FL2M = 100*FT2M         # flight level to meter
+TPSI95 = 10             # Response time in s of 95% of time
+TPHI95 = 1              
+TAUPSI = TPSI95/3
+TAUPHI = TPHI95/3
