9a8ee92eb51fddb04bc96de4612070a46cdd75ab
Soufiane KHAIT <soufiane.khait@alumni.enac.fr>
2021-05-28 13:17:33 +0200

    init global variables

---
+++ config.py
@@ -0,0 +1,9 @@
+from numpy import pi
+
+# Init variables
+g = 9.81                # gravitational constant (m/s)
+DEG2RAD = pi/180        # degree to radian
+FT2M = 0.3048           # feet to meter
+NM2M = 1852             # nautical mile to meter
+KTS2MS = NM2M/3600      # knot to m/s
+FL2M = 100*FT2M         # flight level to meter
