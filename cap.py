d5868f31af720f8277824a827671ac3c56b0c65b
Soufiane KHAIT <soufiane.khait@alumni.enac.fr>
2021-05-28 17:44:13 +0200

    define euler method

--- cap.py
+++ cap.py
@@ -16,14 +16,18 @@ nxe = sin(fpae)                             # load factor X
 nze = cos(fpae)/cos(phie)                   # load factor Z
 pe = 0                                      # roll rate
 
+# time in ms
+t0 = 0
+T = 150
+N = 1000
+
 
 # Equilibrium vector of interest
-xE = np.array([[ve], [fpae], [hdge], [phie]])     # equilibrium state vector 
-uE = np.array([[nxe], [nze], [pe]])               # equilibrium command vector
+xe = np.array([[ve], [fpae], [hdge], [phie]])     # equilibrium state vector 
+ue = np.array([[nxe], [nze], [pe]])               # equilibrium command vector
 
-
+"""
 #Derivatives of multivariable function
- 
 v, gamma, psi, phi = sym.symbols('v gamma psi phi')
 f = g * (nxe - sin(fpa))
  
@@ -33,19 +37,49 @@ print(derivative_f)
 
 
 # Compute State Vector
-def computeStateVector(nx, nz, p):
-    vDot = g * (computeNx() - sin(fpa))
-    gammaDot = g/v * (nz * cos(phi) - cos(fpa))
-    psiDot = g / (v * cos(fpa)) * computeNz * sin(phi)
+
+def computeStateVector(nxe, nz, p):
+    vDot = g * (nx - sin(fpa))
+    fpaDot = g/v * (nz * cos(phi) - cos(fpa))
+    psiDot = g / (v * cos(fpa)) * nz * sin(phi)
     phiDot = p
-    return np.array([[vDot], [gammaDot], [psiDot], [phiDot]])
-
-
-# Integrate State Vector
-def integrateStateVector(*params):
-    v = integrate.quad(vDot, 0, np.inf)
+    return np.array([[vDot], [fpa], [psiDot], [phiDot]])
 
 
 # Capture Heading
 def captureHDG():
     computeStateVector()
+
+"""
+
+
+# Define parameters
+# Functions
+vDot = lambda fpa, nx: g * (nx - sin(fpa))
+fpaDot = lambda fpa, phi, nz, v: g/v * (nz * cos(phi) - cos(fpa))
+psiDot = lambda v, fpa, nz, phi: g / (v * cos(fpa)) * nz * sin(phi)
+phiDot = p
+
+h = (T-t0)/N                      # Step size
+t = np.arange(0, 1 + h, h)        # Numerical grid
+# Initial Conditions
+v0 = ve
+hdg0 = hdge
+fpa0 = fpae
+phi0 = phie
+
+# Explicit Euler Method
+v = np.zeros(len(t))
+fpa = np.zeros(len(t))
+hdg = np.zeros(len(t))
+phi = np.zeros(len(t))
+v[0] = v0
+fpa[0] = fpa0
+hdg[0] = hdg0
+phi[0] = phi0
+
+
+for i in range(0, len(t) - 1):
+    v[i + 1] = v[i] + h*vDot(fpa[i], nxe)
+    fpa[i + 1] = fpa[i] + h*fpaDot(fpa[i], phi[i], nze, v[i])
+    psi[i + 1] = psi[i] + h*psiDot(v[i], fpa[i], nze, phi[i])
