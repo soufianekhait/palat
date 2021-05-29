e694db4fb0b8554a9c7473e198a21df2c040354f
Soufiane KHAIT <soufiane.khait@alumni.enac.fr>
2021-05-29 15:11:44 +0200

    separate files

--- main.py
+++ main.py
@@ -1,34 +1,15 @@
-from ivy.std_api import IvyInit, IvyStart, IvySendMsg, IvyStop
+from ivy.std_api import IvyInit, IvyStart
 from time import sleep
-from receiver import recorded_data  # output dictionnary
-
-app_name = "Sender"
-ivy_bus = "127.255.255.255:2010"
-
-
-def null_cb(*a):
-    pass
+from config import app_name, ivy_bus, null_cb
+from sender import sendData
 
 
 def main():
-    IvyInit(app_name, app_name + " is ready!", 0, null_cb, null_cb)
+    IvyInit(app_name, app_name + " is ready to send!", 0, null_cb, null_cb)
     IvyStart(ivy_bus)
     sleep(1.0)
-
-    while(1):
-        IvySendMsg("APLAT p=3.5")
-        # for test only
-        IvySendMsg("FCULATERAL Mode = SelectedHeading Val =40")
-        IvySendMsg("FCULATERAL Mode = SelectedTrack Val =50")
-        IvySendMsg("FCULATERAL Mode = Managed Val =10")
-        IvySendMsg("StateVector x=33 y=44 z=12 Vp=120 fpa=20 psi=5 phi=5")
-        IvySendMsg("WindComponent VWind=10 dirWind=180")
-        IvySendMsg("MagneticDeclinaison =2")
-        IvySendMsg("RollRateLim  MaxRollRate =66 / MinRollRate =0")
-        IvySendMsg("FGS FgsPt =(120,5)")
-        IvySendMsg("FGS FgsCap =120")
-        sleep(3.0)
-    IvyStop()
+    # send AP LAT data
+    sendData()
 
 
 if __name__ == "__main__":
