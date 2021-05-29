a47b32b062f2d8cab7b20117a277e3ca2348d910
Soufiane KHAIT <soufiane.khait@alumni.enac.fr>
2021-05-29 15:11:20 +0200

    update config

--- config.py
+++ config.py
@@ -1,13 +1,19 @@
 from numpy import pi
 
-# Init variables
+# bus configuration
+app_name = "Lateral Auto Pilot"
+ivy_bus = "127.255.255.255:2010"
+null_cb = lambda *a: None
+
+
+# parameters
 g = 9.81                # gravitational constant (m/s)
 DEG2RAD = pi/180        # degree to radian
 FT2M = 0.3048           # feet to meter
 NM2M = 1852             # nautical mile to meter
 KTS2MS = NM2M/3600      # knot to m/s
 FL2M = 100*FT2M         # flight level to meter
-TPSI95 = 10             # Response time in s of 95% of time
+TPSI95 = 10             # Response time in 95% of time (seconds)
 TPHI95 = 1              
 TAUPSI = TPSI95/3
 TAUPHI = TPHI95/3
